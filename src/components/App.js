12e9cc1c376efe50ba9c86e8ea4e43d38d333d6b
penguyen <peternguyen008@gmail.com>
2018-06-13 12:30:45 -0400

    toggleTags, redefine folder navigation logic - back btn functionality next to do

--- src/components/App.js
+++ src/components/App.js
@@ -3,33 +3,20 @@ import logo from '../logo.svg';
 import '../App.css';
 import tags from '../tags';
 import Items from './Items';
-// import Selected from './Selected';
-// <Selected selected={this.state.selected} />
 
 class App extends Component {
   constructor(props){
     super(props);
-
-    this.state = {
-      selectedTags: []
-    }
   }
 
   showItems = (id = null) => {
-    console.log('FIRST :: showing Root items');
+    // filter json data with parent id
     return tags.map(item => {
       if(item.parent === id){ return item }
     }).filter(x => x !== undefined);
   }
 
-  // componentWillUpdate = () => {
-  //   let rItems = this.showItems();
-  //   this.setState({ history: [...this.state.history, rItems] });
-  //   console.log('History: ', this.state.history);
-  // }
-
   render() {
-    // let showItems = this.showItems();
     return (
       <div className="App">
         <header className="App-header">
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,48 +1,69 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
 import tags from '../tags';
-// import Tag from './Tag';
-// import Folder from './Folder';
 
 class Items extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('scu1: ', nextProps)
-    console.log('scu2: ', nextState)
+  constructor(props){
+    super(props);
 
-    const diffItems = this.props.items !== nextState.items;
-    console.log('scu: ', diffItems)
-    return diffItems;
+    this.state = {
+      counter: 1,
+      history: {0: this.props.showItems()},
+      selectedTags: [],
+      parent: null
+    }
   }
 
   folderClick = (id) => {
-    console.log('going into Folder id: ', id);
+    // get items with new parent id
     let newitems = this.props.showItems(id);
-    console.log('showing Folder: ', newitems);
+    let count = this.state.counter;
     
+    // rerender component with new state
     this.setState((prevState, props) => {
-      console.log('prevState: ', prevState);
-      console.log('props: ', props);
-      return {items: (props.items).splice(0, props.items.length, ...newitems)}
+      return {
+        parent: id,
+        counter: prevState.counter+1,
+        history: { ...prevState.history, [count]: props.items},
+        items: (props.items).splice(0, props.items.length, ...newitems)
+      }
     })
   }
 
-  tagToggle = (id) => {
-    console.log('Toggle tag id: ', id);
-    // toggle selected tag
+  onTagSelectionChange = (id) => {
+    // if tag.id exists in selectedTags[], remove from array. otherwise, add to array
+    this.setState({
+      selectedTags: this.state.selectedTags.indexOf(id) !== -1 ? this.state.selectedTags.splice(+(this.state.selectedTags.indexOf(id)), 1) : this.state.selectedTags.concat(id)
+    })
   }
 
+  isSelected = (id) => {
+    // if tag.id exists in selectedTags[], set className="selected". otherwise, className=""
+    return this.state.selectedTags.includes(id) ? "selected" : ""
+  }
+
+  goBack = () => {
+    // revert back to previous history[counter - 1] -- TODO
+    let count = this.state.counter - 1;
+    let history = this.state.history[count]
+
+    this.setState({
+      history: history,
+      items: history[count]
+    })
+  }
+
+  // Things left to do: Back functionality, Style Components, tagToggle logic
+
   render() {
-    // const { items } = this.props;
-    // let items = this.props.showItems();
-    
     return (
       <div>
+        <div>{this.state.parent !== null ? <button onClick={() => this.goBack.bind(this)}>Go Back</button> : ''}</div>
         <ul>
         {
-          this.props.items.map( (item, index) => {
+          this.props.items.map((item, index) => {
             return (item.isFolder) 
-              ? <li key={`folder-${index}`} value={item.id} onClick={() => this.folderClick(item._id)}> {item.name} </li>
-              : <li key={`tag-${index}`} value={item.id} onClick={() => this.tagToggle(item._id)}> {item.name} </li>
+              ? <li key={`folder-${index}`} onClick={() => this.folderClick(item._id)}> {item.name} </li>
+              : <li key={`tag-${index}`} onClick={() => this.onTagSelectionChange(item._id)} className={this.isSelected(item._id)}> {item.name} </li>
           })
         }
         </ul>
